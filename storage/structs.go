fe595b9e3f7d6ff85f3229b17b6ef8bbf90ea8d9
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-12-03 17:44:32 +0530

    filrpath encoding added

--- storage/structs.go
+++ storage/structs.go
@@ -1,5 +1,12 @@
 package storage
 
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"path/filepath"
+	"strings"
+)
+
 type PathTransformFunc func(string) string
 
 type StorageOptions struct {
@@ -8,4 +15,19 @@ type StorageOptions struct {
 
 var DefaultPathTransformFunc = func(key string) string {
 	return key
+}
+
+var CASPathTransformFunc = func(key string) string {
+	hash := sha1.Sum([]byte(key)) // convert [20] into slice use [:]
+	hashStr := hex.EncodeToString(hash[:])
+	blockSize := 8
+	sliceLen := len(hashStr) / blockSize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blockSize, (i*blockSize)+blockSize
+		paths[i] = hashStr[from:to]
+	}
+
+	return strings.Join(paths, string(filepath.Separator))
 }
