6e6013f6dac52f151fec90c9befa5f87285946be
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-12-29 21:03:13 +0530

    refactor: enhance file server functionality with payload handling and broadcasting

---
+++ fileServer/structs.go
@@ -0,0 +1,20 @@
+package fileserver
+
+import (
+	"github.com/sumit-behera-in/goLogger"
+	"github.com/sumit-behera-in/gonas/p2p"
+	"github.com/sumit-behera-in/gonas/storage"
+)
+
+type Payload struct {
+	Key  string
+	Data []byte
+}
+
+type FileServerOpts struct {
+	Logger            *goLogger.Logger          // this is the global logger
+	StorageRoot       string                    // root storage to store all files or folders managed by goNAS
+	PathTransformFunc storage.PathTransformFunc // used encrypt path and file name
+	Transport         p2p.Transport             // TCP, UDP, HTTP
+	BootStrapNodes    []string
+}
